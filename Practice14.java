9337a096dce4c693f3523f1fe73d960cc91507a2
kawashima nagisa <kawashima.it333@outlook.com>
2023-02-12 12:29:47 +0900

    Edit

---
+++ Practice14.java
@@ -0,0 +1,25 @@
+public class Practice14 {
+    public static void main(String[] args) {
+        // String[] names =  new String[] { "taro", "takashi", "hanako", "maco", "satoshi"};
+
+
+        // String printName = names[3];
+        // System.out.println(printName); 
+
+
+        // String [][] animals = new String[][] {{"アメリカンショートヘアー", "マンチカン"}, {"ジャンガリアンハムスター", "ハリネズミ"}, {"オカメインコ", "文鳥"},{"ゴールデンレトリバー", "トイプードル"}};
+        // System.out.println(animals[3][1]);
+        // String[][] animals = {}
+
+        int input1 = Integer.parseInt(args[0]);
+        int input2 = Integer.parseInt(args[1]);
+        int input3 = Integer.parseInt(args[2]);
+
+        System.out.println(input1 + input2  + input3);
+
+
+
+
+        
+    }
+}
